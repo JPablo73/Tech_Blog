f9b74eb19317c52bb48c6764ad781414f36c4385
JPablo73 <82916926+JPablo73@users.noreply.github.com>
2021-11-06 15:26:44 -0700

    final edit models/views

--- models/User.js
+++ models/User.js
@@ -1,8 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
-const sequelize = require("../config/config");
+const sequelize = require("../config/connection");
 
-// create our User model
 class User extends Model {
   // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
